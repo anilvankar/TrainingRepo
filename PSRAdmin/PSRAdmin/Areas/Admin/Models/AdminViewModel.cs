578f5bb991923231dbfd92d8b3ee5a23f0637804
unknown <anilkumarv@AnilKumarV.cybage.com>
2015-11-03 17:22:39 +0530

    added model

---
+++ PSRAdmin/PSRAdmin/Areas/Admin/Models/AdminViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+using System.ComponentModel.DataAnnotations;    
+
+namespace PSRAdmin.Areas.Admin.Models
+{
+    public class AdminViewModel
+    {
+     
+        [Required(ErrorMessage="Name is required.")]               
+        public string EmployeeName { get; set; }
+
+       
+        public string EmployeeAddress { get; set; }
+      
+    }
+}
