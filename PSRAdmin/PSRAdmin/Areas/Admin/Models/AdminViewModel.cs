aaacdf0b0448442799c723151e5e59e4a505d7d4
Anilkumar Vankar <anilkumarv@cybage.com>
2015-11-04 14:33:43 +0530

    Conflicts: 	PSRAdmin/PSRAdmin.v11.suo

---
+++ PSRAdmin/PSRAdmin/Areas/Admin/Models/AdminViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+using System.ComponentModel.DataAnnotations;    
+
+namespace PSRAdmin.Areas.Admin.Models
+{
+    public class AdminViewModel
+    {
+     
+        [Required(ErrorMessage="Name is required.")]               
+        public string EmployeeName { get; set; }
+
+       [StringLength(50)];
+        public string EmployeeAddress { get; set; }
+      
+    }
+}
---
+++ PSRAdmin/PSRAdmin/MSBuild1.bat
@@ -0,0 +1,5 @@
+msbuild.sonarqube.runner.exe begin /k:"PSRAdmin" /n:"PSRAdmin" /v:"1.1"
+
+msbuild
+
+msbuild.sonarqube.runner.exe end
