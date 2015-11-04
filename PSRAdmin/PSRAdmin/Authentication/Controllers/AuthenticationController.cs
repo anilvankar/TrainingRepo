9d163ce4195c72b279bc4f327b8eff702010eccb
bhavnak <bhavnak@cybage.com>
2015-11-04 09:58:16 +0530

    commit before creating another branch

---
+++ PSRAdmin/PSRAdmin/Authentication/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace PSRAdmin.Authentication.Controllers
+{
+    public class AuthenticationController:Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ PSRAdmin/PSRAdmin/Authentication/Models/UserDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSRAdmin.Authentication.Models
+{
+    public class UserDetails
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+
+
+
