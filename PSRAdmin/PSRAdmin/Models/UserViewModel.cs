9e96e2697ed289dbe086f47eda5ed19d71022bfa
anilvankar <anilkumarv@cybage.com>
2015-11-04 14:23:04 +0530

    last commit

--- PSRAdmin/PSRAdmin/Models/UserViewModel.cs
+++ PSRAdmin/PSRAdmin/Models/UserViewModel.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PSRAdmin.Models
 {
     public class UserViewModel
     {
+        [Required]
         public string UserName;
 
+        [RegularExpression("")]
         public string EmailAddress;
 
+        [StringLength(50)]
         public string Address;
+
     }
 }
