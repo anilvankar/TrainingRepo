d6025e0c1c7aca56902654856fcdc20d069bf924
anilvankar <anilkumarv@cybage.com>
2015-11-04 14:08:57 +0530

    Added User View Model

---
+++ PSRAdmin/PSRAdmin/Models/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PSRAdmin.Models
+{
+    public class UserViewModel
+    {
+        public string UserName;
+    }
+}
